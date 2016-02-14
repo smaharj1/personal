b5674ffab2256a75072d6d3f91170c6d09000958
Sujil Maharjan <smaharj1@ramapo.edu>
2016-02-14 00:11:39 -0500

    Carousel caption added

---
+++ public/javascripts/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#hire').on('click', function () {
+        window.open('mailto:smaharj1@ramapo.edu','_blank');
+    });
+    
+    $('#details').on('click',function () {
+        window.open('#portfolio','_self');
+    });
+});
