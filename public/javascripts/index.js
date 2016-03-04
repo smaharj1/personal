2554a4cc72174d5230fe67dc61e8e78ced2bf746
Sujil Maharjan <smaharj1@ramapo.edu>
2016-03-04 17:40:39 -0500

    Added a new block in the page with the portfolio with basic design. If you click the main positions held, it slides in more info on the clicked row

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     $('#hire').on('click', function () {
         window.open('mailto:smaharj1@ramapo.edu','_blank');
@@ -6,4 +8,22 @@ $(document).ready(function () {
     $('#details').on('click',function () {
         window.open('#portfolio','_self');
     });
+    
+    $('.row').on('click', function () {
+        var info = $(this).children('.info');
+        if (info.data('isHidden') == 'true') {
+            info.slideDown();
+            info.data('isHidden','false');
+        }
+        else {
+            info.slideUp();
+            info.data('isHidden','true');
+        }
+    })
+    
+    var rows = $('.row').children('.info').each(function () {
+        $(this).hide();
+        $(this).data('isHidden','true');
+    });
+   
 });
